3c54831c017e8840c05e7f845c460b55c3f8f143
Nico Morell <nicomorell@outlook.com>
2018-06-19 12:14:09 +0100

    Top 5 viewed pages exercise

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,41 +12,50 @@ def populate():
 
     add_page(cat=python_cat,
              title="Official Python Tutorial",
-             url="http://docs.python.org/2/tutorial/")
-
+             url="http://docs.python.org/2/tutorial/",
+	     views = 150)
+		
     add_page(cat=python_cat,
              title="How to Think like a Computer Scientist",
-             url="http://www.greenteapress.com/thinkpython/")
+             url="http://www.greenteapress.com/thinkpython/",
+	     views = 130)
 
     add_page(cat=python_cat,
              title="Learn Python in 10 Minutes",
-             url="http://www.korokithakis.net/tutorials/python/")
+             url="http://www.korokithakis.net/tutorials/python/",
+	     views = 145)
 
     django_cat = add_cat("Django", views = 64, likes = 32)
 
 
     add_page(cat=django_cat,
              title="Official Django Tutorial",
-             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+	     views = 123)
 
     add_page(cat=django_cat,
              title="Django Rocks",
-             url="http://www.djangorocks.com/")
+             url="http://www.djangorocks.com/",
+	     views = 34)
 
     add_page(cat=django_cat,
              title="How to Tango with Django",
-             url="http://www.tangowithdjango.com/")
+             url="http://www.tangowithdjango.com/",
+	     views = 453)
+
 
     frame_cat = add_cat("Other Frameworks", views = 32, likes = 16)
 
 
     add_page(cat=frame_cat,
              title="Bottle",
-             url="http://bottlepy.org/docs/dev/")
+             url="http://bottlepy.org/docs/dev/",
+	     views = 1440)
 
     add_page(cat=frame_cat,
              title="Flask",
-             url="http://flask.pocoo.org")
+             url="http://flask.pocoo.org",
+	     views = 2)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
@@ -54,7 +63,7 @@ def populate():
             print "- {0} - {1}".format(str(c), str(p))
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
